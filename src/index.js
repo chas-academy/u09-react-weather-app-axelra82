2d258bcbc98b6a311059b94543e1cbfe9458fd3e
Axel Roussille Åberg <axelra82@gmail.com>
2021-04-17 15:31:19 +0200

    chore: include store context in main entry point

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './layouts/main';
+import ContextProvider from './provider/ContextProvider';
+import './index.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<ContextProvider>
+			<App />
+		</ContextProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
