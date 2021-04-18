6d256494b2ddd70d812b275567c53a2a4898774d
Axel Roussille Åberg <axelra82@gmail.com>
2021-04-18 12:37:14 +0200

    chore: install react-dotenv for propper env var handling

--- src/api/open-weather-map/calls/geolocation.js
+++ src/api/open-weather-map/calls/geolocation.js
@@ -1,10 +1,14 @@
 /* eslint-disable import/no-anonymous-default-export */
-/* eslint-disable default-case */
 import axios from 'axios';
+import env from 'react-dotenv';
 
 export default async (query) => {
-	const apiUrl = process.env.REACT_APP_OWM_API_URL;
-	const apiKey = process.env.REACT_APP_OWM_API_KEY;
+    // Make sure to NOT USE default react-scripts for environment
+	// handling on secret variables (e.g. API keys etc) as they will
+	// be visible in frontend after build:
+    // https://create-react-app.dev/docs/adding-custom-environment-variables/
+	const apiUrl = env.REACT_APP_OWM_API_URL;
+	const apiKey = env.REACT_APP_OWM_API_KEY;
 	let response;
 	
 	try {
@@ -20,7 +24,6 @@ export default async (query) => {
 		const request = await axios(options);
 		
 		// Success object
-		// request.data = JSON.parse(request.data);
 		response = request.data
 
 	} catch (error) {
