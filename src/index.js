eadc6941fa8d82550376b52ad60afbbfdfea0cc7
Axel Roussille Åberg <axelra82@gmail.com>
2021-04-19 08:26:26 +0200

    chore: include main styling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Main, Header, Footer } from './layout';
 import ContextProvider from './provider/ContextProvider';
-import './index.scss';
+import './styling/style.scss';
 
 ReactDOM.render(
 	<React.StrictMode>
