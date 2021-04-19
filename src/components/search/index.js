91a008a3a0cb428d3e00f928ab239278393b78e6
Axel Roussille Åberg <axelra82@gmail.com>
2021-04-19 13:18:35 +0200

    chore: move init api to main (from search)

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -7,77 +7,77 @@ import './style.scss';
 
 export default () => {
 
-	const {
-		store: {
-			location: {
-				lat,
-				lon,
-				setLat,
-				setLon,
-				setCountry,
-				setName,
-			}
-		}
-	} = useContext(StoreContext);
+	// const {
+	// 	store: {
+	// 		location: {
+	// 			lat,
+	// 			lon,
+	// 			setLat,
+	// 			setLon,
+	// 			setCountry,
+	// 			setName,
+	// 		}
+	// 	}
+	// } = useContext(StoreContext);
 
-	useEffect(() => {
-		const locationName = async() => {
-			// Make static durring dev
-			// const settings = {
-			// 	method: 'post',
-			// 	url: '/.netlify/functions/openweathermap',
-			// 	data: {
-			// 		direction: 'reverse',
-			// 		search: {
-			// 			lat,
-			// 			lon
-			// 		}
-			// 	}
-			// };
+	// useEffect(() => {
+	// 	const locationName = async() => {
+	// 		// Make static durring dev
+	// 		// const settings = {
+	// 		// 	method: 'post',
+	// 		// 	url: '/.netlify/functions/openweathermap',
+	// 		// 	data: {
+	// 		// 		direction: 'reverse',
+	// 		// 		search: {
+	// 		// 			lat,
+	// 		// 			lon
+	// 		// 		}
+	// 		// 	}
+	// 		// };
 			
-			// const getLocationName = await axios(settings);
-			// const firstResult = getLocationName.data;
+	// 		// const getLocationName = await axios(settings);
+	// 		// const firstResult = getLocationName.data;
 			
-			// setCountry(firstResult.country);
-			// setName(firstResult.name);
+	// 		// setCountry(firstResult.country);
+	// 		// setName(firstResult.name);
 			
-			setCountry('Sweden');
-			setName('Stockholm');
-		}
+	// 		setCountry('Sweden');
+	// 		setName('Stockholm');
+	// 	}
 
-		const geoSuccess = position => {
-			setLat(position.coords.latitude);
-			setLon(position.coords.longitude);
-		}
+	// 	const geoSuccess = position => {
+	// 		setLat(position.coords.latitude);
+	// 		setLon(position.coords.longitude);
+	// 	}
 
-		const geoError = error => {
-			if(error.code === error.PERMISSION_DENIED){
-				alert('Please allow application to view your current position for precise location matching. You may also use the location search function.');
-			}
-		}
+	// 	const geoError = error => {
+	// 		if(error.code === error.PERMISSION_DENIED){
+	// 			alert('Please allow application to view your current position for precise location matching. You may also use the location search function.');
+	// 		}
+	// 	}
 
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+	// 	if (navigator.geolocation) {
+	// 		navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
 
-			if(lat && lon){
-				locationName();
-			}
-		}else{
-			alert('Geolocation does not appear to be supported in this browser.');
-		}
+	// 		if(lat && lon){
+	// 			locationName();
+	// 		}
+	// 	}else{
+	// 		alert('Geolocation does not appear to be supported in this browser.');
+	// 	}
 
-		// Default values, i.e. no input
-		if(lat === 0 && lon === 0){
+	// 	// Default values, i.e. no input
+	// 	if(lat === 0 && lon === 0){
 
-		}
+	// 	}
 
-		/* eslint-disable-next-line react-hooks/exhaustive-deps */
-	}, [lat,lon]);
+	// 	/* eslint-disable-next-line react-hooks/exhaustive-deps */
+	// }, [lat,lon]);
 
 	const doSearch = async (e) => {
 		e.preventDefault();
 		const str = e.target.childNodes[0].value;
-		// console.log(str);
+		console.log(str);
 
 		if(str) {
 			// const location = await getGeoLocation('direct', str);
--- src/layout/main/index.js
+++ src/layout/main/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React, { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import StoreContext from '../../context/StoreContext';
+import axios from 'axios';
+
 import { Header, Footer } from '../';
 import Weather from '../../components/weather';
 
@@ -9,17 +11,74 @@ import './style.scss';
 export default () => {
 	
 	const {
-		store:{
-	        classes: {
+		store: {
+			location: {
+				lat,
+				lon,
+				setLat,
+				setLon,
+				setCountry,
+				setName,
+			},
+			classes: {
                 tod,
                 gradient
             },
 		}
 	} = useContext(StoreContext);
 
+	useEffect(() => {
+		const locationName = async() => {
+			// Make static durring dev
+			// const settings = {
+			// 	method: 'post',
+			// 	url: '/.netlify/functions/openweathermap',
+			// 	data: {
+			// 		direction: 'reverse',
+			// 		search: {
+			// 			lat,
+			// 			lon
+			// 		}
+			// 	}
+			// };
+			
+			// const getLocationName = await axios(settings);
+			// const firstResult = getLocationName.data;
+			
+			// setCountry(firstResult.country);
+			// setName(firstResult.name);
+			
+			setCountry('Sweden');
+			setName('Stockholm');
+		}
+
+		const geoSuccess = position => {
+			setLat(position.coords.latitude);
+			setLon(position.coords.longitude);
+		}
+
+		const geoError = error => {
+			if(error.code === error.PERMISSION_DENIED){
+				alert('Please allow application to view your current position for precise location matching. You may also use the location search function.');
+			}
+		}
+
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+
+			if(lat && lon){
+				locationName();
+			}
+		}else{
+			alert('Geolocation does not appear to be supported in this browser.');
+		}
+
+		/* eslint-disable-next-line react-hooks/exhaustive-deps */
+	}, [lat,lon]);
+
 	return(
 		<section className={`gradient ${tod} ${gradient}`}>
-			<Header />
+			{/* <Header /> */}
 			
 			<main className='container'>
 				<Weather />
