9c005e18dd7606ad3ce99e0dbab99c2df271480b
Axel Roussille Åberg <axelra82@gmail.com>
2021-04-19 13:19:27 +0200

    chore: put unit switch in footer

--- src/layout/footer/index.js
+++ src/layout/footer/index.js
@@ -1,12 +1,14 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
+import UnitSwitch from '../../components/unit-switch';
+
 import './style.scss';
 
 export default () => {
 
     return(
-        <h1>
-            Footer
-        </h1>
+        <footer>
+            <UnitSwitch />
+        </footer>
     )
 }
