d796f76a46470abc2ad69abb01811532cde96d25
Axel Roussille Åberg <axelra82@gmail.com>
2021-04-20 11:09:03 +0200

    chore: update localStorage helper with comments and loop getter

--- src/helpers/localstore.js
+++ src/helpers/localstore.js
@@ -1,14 +1,28 @@
+// Reusable local storage item name
 const localStorageItem = 'u09owm_axelra82';
+// Default data for unit and saved locations
+const defaultData = {
+    unit: 'metric',
+    locations: [],
+};
 
 const setLocal = (data) => {
     if(typeof data === 'object'){
-        localStorage.setItem(localStorageItem, data);
+        localStorage.setItem(localStorageItem, JSON.stringify(data));
     }
     return;
 }
 
 const getLocal = () => {
-    return localStorage.getItem(localStorageItem);
+    const hasLocal = localStorage.getItem(localStorageItem);
+    // Make sure we have localStorage data
+    // If not, set default data and run again
+    if(hasLocal){
+        return JSON.parse(hasLocal);
+    }else{
+        setLocal(defaultData);
+        getLocal();
+    }
 }
 
 export {
