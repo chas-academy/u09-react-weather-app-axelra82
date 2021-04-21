f6c595645edea0ad24dccc2e252f4592ba578902
Axel Roussille Åberg <axelra82@gmail.com>
2021-04-21 18:13:41 +0200

    chore: update search and put in overlay

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import StoreContext from '../../context/StoreContext';
 import axios from 'axios';
 
@@ -7,18 +7,18 @@ import './style.scss';
 
 export default () => {
 
-	// const {
-	// 	store: {
-	// 		location: {
-	// 			lat,
-	// 			lon,
-	// 			setLat,
-	// 			setLon,
-	// 			setCountry,
-	// 			setName,
-	// 		}
-	// 	}
-	// } = useContext(StoreContext);
+	const {
+		store: {
+			setSearchLocation,
+			setMenuOpen,
+			location: {
+				setLat,
+				setLon,
+				setCountry,
+				setName,
+			}
+		}
+	} = useContext(StoreContext);
 
 	// useEffect(() => {
 	// 	const locationName = async() => {
@@ -74,27 +74,88 @@ export default () => {
 	// 	/* eslint-disable-next-line react-hooks/exhaustive-deps */
 	// }, [lat,lon]);
 
+	const [noResults, setNoResults] = useState(false);
+	
+	const getGeoLocation = async(str) => {
+
+		const settings = {
+			method: 'post',
+			url: '/.netlify/functions/owm-location',
+			data: {
+				direction: 'direct',
+				search:{
+					str
+				}
+			}
+		};
+		
+		const response = await axios(settings);
+		return response.data;
+	}
+
 	const doSearch = async (e) => {
+		// Prevent form from default action
 		e.preventDefault();
-		const str = e.target.childNodes[0].value;
+		const searchField = e.target.childNodes[0];
+		const str = searchField.value;
+		
 		console.log(str);
-
+		
 		if(str) {
-			// const location = await getGeoLocation('direct', str);
-			// console.log(location);
+			setSearchLocation(true);
+			const location = await getGeoLocation(str);
+			console.log('------IN SEARCH------');
+			console.log(location);
+			console.log('------------');
 
-			// if(typeof location !== 'undefined'){
-			// 	if(location.length > 1){
-			// 		// Found more
-			// 	}
-			// }
+			if(typeof location !== 'undefined' || location.length !== 0){
+				const {lat, lon, name, country } = location[0];
+				if(location.length > 1){
+					// Found more
+					console.log('Also found more');
+					// We got exactly one hit
+					setLat(lat);
+					setLon(lon);
+					setCountry(country);
+					setName(name);
+					setMenuOpen(false);
+				}else{
+					// We got exactly one hit
+					setLat(lat);
+					setLon(lon);
+					setCountry(country);
+					setName(name);
+					setMenuOpen(false);
+				}
+			}else{
+				setNoResults(true);
+			}
+			
+		}else{
+			// Notify about no string	
 		}
+
+		
+	}
+
+	const clearNotice = () => {
+		// Reset any potential previous warnings
+		setNoResults(false);
 	}
 
 	return(
-		<form onSubmit={doSearch} id='location-search'>
-			<input name='searchString' type='text' placeholder='Location' />
-			<button type='submit'>Search</button>
-		</form>
+		<>
+			<form onFocus={clearNotice} onSubmit={doSearch} id='location-search'>
+				<input name='searchString' type='text' placeholder='Location' />
+				<button type='submit'>Search</button>
+			</form>
+
+			{/* Inform user that we couldn't find anything for that search string */}
+			{noResults &&
+				<article className='text-center mt-2'>
+					<h1 className='h5'>Sorry, we couldn't find any location matcing that name</h1>
+				</article>
+			}
+		</>
 	);
 }
