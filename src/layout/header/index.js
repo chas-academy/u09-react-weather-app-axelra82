30720cafe36cc00ab4caa0fecd2680956136f02a
Axel Roussille Åberg <axelra82@gmail.com>
2021-04-21 18:18:00 +0200

    chore: update header with new components

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -1,15 +1,20 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
 
+import Burger from '../../components/burger';
+import UnitSwitch from '../../components/unit-switch';
+import LocationOverlay from '../../components/location-overlay';
+
 import './style.scss';
 
 export default () => {
 
+
     return(
         <header>
-            <section className='container'>
-                
-		    </section>
+            <Burger />
+            <UnitSwitch />
+            <LocationOverlay />
         </header>
     );
 }
