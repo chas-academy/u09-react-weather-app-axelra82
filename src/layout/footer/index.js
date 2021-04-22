63b5158f6bbe24eaa67e7df71249ef346d23950a
Axel Roussille Åberg <axelra82@gmail.com>
2021-04-22 21:53:15 +0200

    chore: add credits in footer

--- src/layout/footer/index.js
+++ src/layout/footer/index.js
@@ -9,6 +9,9 @@ export default () => {
         <footer id="main-footer">
             <article className='container ts-small fw-thin text-center'>
                 <p>
+                    CREDITS <a href='https://openweathermap.org/api'>@OPENWEATHERMAP.ORG</a> | <a href='https://erikflowers.github.io/weather-icons/'>@ERIKFLOWERS.GITHUB.IO</a>
+                </p>
+                <p>
                     REPO <a href='https://github.com/axelra82/weather-app-ca-u09'>@GITHUB</a> | CHAS ACADEMY ASSIGNMENT U09 | AXEL ROUSSILLE ÅBERG 2021
                 </p>
             </article>
