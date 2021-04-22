f9ef4f6f37299642c4a1de52ebdb72e53f56db7c
Axel Roussille Åberg <axelra82@gmail.com>
2021-04-22 12:21:52 +0200

    chore: clean up and add multiple results list

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -3,6 +3,8 @@ import { useContext, useState } from 'react';
 import StoreContext from '../../context/StoreContext';
 import axios from 'axios';
 
+import { twoDecimal, countrCodeName } from '../../helpers';
+
 import './style.scss';
 
 export default () => {
@@ -20,62 +22,16 @@ export default () => {
 		}
 	} = useContext(StoreContext);
 
-	// useEffect(() => {
-	// 	const locationName = async() => {
-	// 		// Make static durring dev
-	// 		// const settings = {
-	// 		// 	method: 'post',
-	// 		// 	url: '/.netlify/functions/openweathermap',
-	// 		// 	data: {
-	// 		// 		direction: 'reverse',
-	// 		// 		search: {
-	// 		// 			lat,
-	// 		// 			lon
-	// 		// 		}
-	// 		// 	}
-	// 		// };
-			
-	// 		// const getLocationName = await axios(settings);
-	// 		// const firstResult = getLocationName.data;
-			
-	// 		// setCountry(firstResult.country);
-	// 		// setName(firstResult.name);
-			
-	// 		setCountry('Sweden');
-	// 		setName('Stockholm');
-	// 	}
+	const defaultStates = {
+		resultCount: 0,
+		results: [],
+		noString: false,
+		noResults: false,
+		multipleResults: false,
+	}
 
-	// 	const geoSuccess = position => {
-	// 		setLat(position.coords.latitude);
-	// 		setLon(position.coords.longitude);
-	// 	}
+	const [states, setStates] = useState(defaultStates);
 
-	// 	const geoError = error => {
-	// 		if(error.code === error.PERMISSION_DENIED){
-	// 			alert('Please allow application to view your current position for precise location matching. You may also use the location search function.');
-	// 		}
-	// 	}
-
-	// 	if (navigator.geolocation) {
-	// 		navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
-
-	// 		if(lat && lon){
-	// 			locationName();
-	// 		}
-	// 	}else{
-	// 		alert('Geolocation does not appear to be supported in this browser.');
-	// 	}
-
-	// 	// Default values, i.e. no input
-	// 	if(lat === 0 && lon === 0){
-
-	// 	}
-
-	// 	/* eslint-disable-next-line react-hooks/exhaustive-deps */
-	// }, [lat,lon]);
-
-	const [noResults, setNoResults] = useState(false);
-	
 	const getGeoLocation = async(str) => {
 
 		const settings = {
@@ -93,67 +49,111 @@ export default () => {
 		return response.data;
 	}
 
-	const doSearch = async (e) => {
+	const doSearch = async(e) => {
 		// Prevent form from default action
 		e.preventDefault();
+		
+		// Reset on each search
+		setStates(defaultStates);
+
 		const searchField = e.target.childNodes[0];
 		const str = searchField.value;
-		
-		console.log(str);
-		
+
 		if(str) {
 			setSearchLocation(true);
 			const location = await getGeoLocation(str);
-			console.log('------IN SEARCH------');
-			console.log(location);
-			console.log('------------');
 
-			if(typeof location !== 'undefined' || location.length !== 0){
-				const {lat, lon, name, country } = location[0];
+			if(location.length > 0){
+
 				if(location.length > 1){
 					// Found more
-					console.log('Also found more');
-					// We got exactly one hit
-					setLat(lat);
-					setLon(lon);
-					setCountry(country);
-					setName(name);
-					setMenuOpen(false);
+					setStates({
+						...states,
+						results: location,
+						resultCount: location.length,
+						multipleResults: true,
+					});
 				}else{
 					// We got exactly one hit
+					const {lat, lon, name, country } = location[0];
 					setLat(lat);
 					setLon(lon);
 					setCountry(country);
 					setName(name);
 					setMenuOpen(false);
 				}
-			}else{
-				setNoResults(true);
-			}
-			
-		}else{
-			// Notify about no string	
-		}
 
-		
+				searchField.value = '';
+			}else{
+				setStates({
+					...states,
+					noResults: true,
+				});
+			}	
+		}else{
+			// Notify about no string
+			setStates({
+				...states,
+				noString: true,
+			});
+		}
 	}
 
-	const clearNotice = () => {
-		// Reset any potential previous warnings
-		setNoResults(false);
+	// On click handler for multiple results
+	const getSelectedResult = (result) => {
+		const {lat, lon, name, country } = result;
+		setLat(lat);
+		setLon(lon);
+		setCountry(country);
+		setName(name);
+		setMenuOpen(false);
+		resetStates();
+	}
+
+	const resetStates = () => {
+		// Reset all states
+		setStates(defaultStates);
 	}
 
 	return(
 		<>
-			<form onFocus={clearNotice} onSubmit={doSearch} id='location-search'>
+			<form onFocus={resetStates} onSubmit={doSearch} id='location-search'>
 				<input name='searchString' type='text' placeholder='Location' />
 				<button type='submit'>Search</button>
 			</form>
-
-			{/* Inform user that we couldn't find anything for that search string */}
-			{noResults &&
-				<article className='text-center mt-2'>
-					<h1 className='h5'>Sorry, we couldn't find any location matcing that name</h1>
+			{/* Inform user */}
+			{(states.noResults || states.multipleResults || states.noString) &&
+				<article className='text-center mt-1'>
+					<h1 className='h5'>
+						{states.noString &&
+							<>Please enter a city name to search for</>
+						}
+						{states.noResults &&
+							<>Sorry, we couldn't find any location matcing that name</>
+						}
+						{states.multipleResults &&
+							<>
+								<p>We found {states.resultCount} locations matching your search</p>
+								<span className='ts-small'>Click on the location to get weather</span>
+								<ul id='multiple-results-list' className='mt-1'>
+								{states.results.map((item, idx) => {
+									const {lat, lon, name, country} = item;
+									return(
+										<li
+											key={`multiple-results-list-item-${idx}`}
+											onClick={() => getSelectedResult(item)}
+										>
+											{name} <span className='ts-small'>({countrCodeName(country).name})</span>
+											<div className='ts-small'>
+												lat: {twoDecimal(lat)}, lon: {twoDecimal(lon)}
+											</div>
+										</li>
+									);
+								})}
+								</ul>
+							</>
+						}
+					</h1>
 				</article>
 			}
 		</>
